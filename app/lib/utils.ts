7672ac6ca2e336b93fe86f4d40716fd6320f52b9
Suwantha Darshana <99712377+SuwanthaDarshana@users.noreply.github.com>
2026-03-25 23:55:00 +0530

    update

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -4,3 +4,5 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+// This file can be used for any utility functions that you want to use across your app.
